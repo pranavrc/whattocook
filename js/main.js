dd88d601cf4978446958ef3915b0ca2932c06698
Pranav Ravichandran <me@onloop.net>
2016-03-07 13:53:52 -0500

    Jquery tagsinput import

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 require.config({
     paths: {
         'jquery': '../bower_components/jquery/dist/jquery',
+        'jquery.tagsinput': '../bower_components/jquery.tagsinput/src/jquery.tagsinput',
         'backbone': '../bower_components/backbone/backbone',
         'underscore': '../bower_components/underscore/underscore'
     }
