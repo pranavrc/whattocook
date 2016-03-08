f7efd9eef87a7310ca6590b9197c8aa654f98f0f
Pranav Ravichandran <me@onloop.net>
2016-03-07 19:51:56 -0500

    Require backbone.localStorage

--- js/main.js
+++ js/main.js
@@ -3,11 +3,13 @@ require.config({
         'jquery': '../bower_components/jquery/dist/jquery',
         'jquery.tagsinput': '../bower_components/jquery.tagsinput/src/jquery.tagsinput',
         'backbone': '../bower_components/backbone/backbone',
-        'underscore': '../bower_components/underscore/underscore'
+        'underscore': '../bower_components/underscore/underscore',
+        'backbone.localStorage': '../bower_components/backbone.localstorage/backbone.localStorage'
     },
 
     shim: {
-        'jquery.tagsinput': ['jquery']
+        'jquery.tagsinput': ['jquery'],
+        'backbone.localStorage': ['backbone', 'underscore']
     }
 });
 
