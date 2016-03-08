4454b6fe8f7f322dd587d0e7bc0957a51592872e
Pranav Ravichandran <me@onloop.net>
2016-03-07 19:16:53 -0500

    Functionality for callbacks tagAdded and tagRemoved

--- js/app.js
+++ js/app.js
@@ -21,27 +21,35 @@ define(['jquery',
     var SearchTags = Backbone.Model.extend({
         defaults: {
             tags: []
-        },
+        }
     });
 
     var App = Backbone.View.extend({
         el: '#container',
         
         initialize: function () {
+            _.bindAll(this, 'tagAdded', 'tagRemoved');
+
+            var self = this;
+            this.recipeCollection = new Recipes;
+            this.searchTags = new SearchTags;
+
             this.$el.find('.search-input').tagsInput({
                 'defaultText': 'Search',
                 'width': '99%',
                 'height': '100%',
                 'delimiter': [',',';'],
-                'onAddTag': this.tagAdded,
-                'onRemoveTag': this.tagRemoved
+                'onAddTag': self.tagAdded,
+                'onRemoveTag': self.tagRemoved
             });
         },
 
         tagAdded: function (tag) {
+            this.searchTags.attributes.tags.push(tag);
         },
 
         tagRemoved: function (tag) {
+            this.searchTags.attributes.tags.pop();
         },
         
         render: function () {
